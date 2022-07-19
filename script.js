e5140464c1e09843523339ad4a56195afa87a45e
KnowledgeCat247 <96559252+KnowledgeCat247@users.noreply.github.com>
2022-07-19 11:05:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const nameInput = document.getElementById("nameInput");
+const button = document.getElementById("enter");
+const buttonClick = button.addEventListener("click", nameCat);
+
+
+
+
+
+class Cats 
+{
+  constructor(breed, age)
+  {
+    this.breed = breed;
+    this.age = age;
+  }
+}
+
+class YourCat extends Cats
+{
+  constructor(breed, age, name)
+  {
+    super(breed, age);
+    this.name = name;
+  }
+}
+
+let kitty = new yourCat("Calico", 15, "Orange");
+
+function nameCat()
+{
+  kitty.name = nameInput.value;
+
+  const text1 = document.createElement("p");
+  const text2 = document.createElement("p");
+  const text3 = document.createElement("p");
+
+  text1.innerText = "The breed of the cat is: " + kitty.breed;
+  text2.innerText = "The cat's age is " + kitty.age + " years old";
+  text3.innerText = "You name the cat " + kitty.name;
+
+  document.body.appendChild(text1);
+  document.body.appendChild(text2);
+  document.body.appendChild(text3);
+}
